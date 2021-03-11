8d752fb5183407020ab2b1f07c31a845d127dc2e
Edith M <info@paperhearts.tech>
2021-03-10 18:22:42 -0600

    Sharing options updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,9 +13,9 @@ const Header = () => {
                     <div className="subtitle">
                         <p className="lead">Listado de proveedores de oxígeno (tanques y/o concentradores) en Monterrey y área metropolitana.</p>
                         <p className="social">Compartir en 
-                            <a   href="https://www.facebook.com/sharer/sharer.php?u=PAGE_URL" target="_blank" rel="noreferrer" title="Compartir en Facebook"><FontAwesomeIcon icon={["fab", "facebook"]} className="social-icon"/></a>
+                            <a   href="https://www.facebook.com/sharer/sharer.php?u=https://oxigenomty.cc" target="_blank" rel="noreferrer" title="Compartir en Facebook"><FontAwesomeIcon icon={["fab", "facebook"]} className="social-icon"/></a>
                             <a   href="https://api.whatsapp.com/send?text=https://oxigenomty.cc" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={["fab", "whatsapp"]} /></a>
-                            <a   href="https://twitter.com/intent/tweet?text=PAGE_TITLE&amp;url=PAGE_URL"  target="_blank" rel="noreferrer" title="Compartir en Twitter"><FontAwesomeIcon icon={["fab", "twitter"]} /></a>
+                            <a   href="https://twitter.com/intent/tweet?text=Oxígeno MTY&amp;url=https://oxigenomty.cc"  target="_blank" rel="noreferrer" title="Compartir en Twitter"><FontAwesomeIcon icon={["fab", "twitter"]} /></a>
                         </p>
                         <p className="warning">
                             <a href="https://www.forbes.com.mx/noticias-consejos-para-prevenir-fraudes-en-venta-o-renta-de-tanques-de-oxigeno/" target="_blank" rel="noreferrer">
